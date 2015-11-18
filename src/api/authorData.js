24c1982310ce255e3def7ae8dbbbcfb52ef37dae
Rostislav <volskyirostislav@gmail.com>
2015-11-18 10:09:21 +0200

    Added mock api and authors page

---
+++ src/api/authorData.js
@@ -0,0 +1,20 @@
+module.exports = {
+  authors:
+    [
+      {
+        id: 'hello-world',
+        firstName: 'hello',
+        lastName: 'world'
+      },
+      {
+        id: 'hello-universe',
+        firstName: 'Hello',
+        lastName: 'Universe'
+      },
+      {
+        id: 'another-hello',
+        firstName: 'Another',
+        lastName: 'Hello'
+      }
+    ]
+};
